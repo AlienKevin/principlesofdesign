a407858f2f2d30c3b55519302454656f160bffef
Kevin Li <kevinli020508@gmail.com>
2022-04-18 19:45:45 -0400

    Add header to home page

--- _includes/scripts/home.js
+++ _includes/scripts/home.js
@@ -1,3 +1,5 @@
-/*(function () {
-
-})();*/
+(function () {
+	let intro = document.createElement("h1");
+	intro.textContent = "Universal Principles of Design is a set of best practices to help you design better products and services.";
+	document.querySelector(".page__content > .main").prepend(intro);
+})();
