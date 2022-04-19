a8b55224d51f87ea5932bc6ba38d872aedbf04a9
Kevin Li <kevinli020508@gmail.com>
2022-04-18 20:49:08 -0400

    Reduce intro <p> font size and add spacing

--- _includes/scripts/home.js
+++ _includes/scripts/home.js
@@ -1,5 +1,6 @@
 (function () {
-	let intro = document.createElement("h1");
+	let intro = document.createElement("p");
+	intro.style = "font-size: 1.5rem; margin-top: 4rem;";
 	intro.textContent = "Universal Principles of Design is a set of best practices to help you design better products and services.";
 	document.querySelector(".page__content > .main").prepend(intro);
 })();
